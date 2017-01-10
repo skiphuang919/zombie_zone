15ae08f0c9b434adbfa453dd051d9e922d38ca88
huangkai <huangkai919@163.com>
2017-01-10 22:14:21 +0800

    add wechat oauth2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from form import NameForm
@@ -8,6 +8,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+
+@app.route('/oauth2/', methods=['GET', 'POST'])
+def wc_oauth2():
+    code = request.args.get('code', None)
+    pass
 
 
 @app.route('/', methods=['GET', 'POST'])
