1f489dc82813029d44a95dbf1d8653d12cdab83e
ubuntu <huangkai919@163.com>
2017-02-08 18:16:51 +0800

    add validater msg

--- app/main/form.py
+++ app/main/form.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class OrderForm(FlaskForm):
-    contacts = StringField(u'contacts', validators=[DataRequired(), ])
-    tel = StringField(u'tel', validators=[DataRequired(), Length(max=11)])
-    store_name = StringField(u'store name')
-    store_addr = StringField(u'store addr', validators=[Length(max=64)])
-    device_id = StringField(u'device id', validators=[DataRequired(), Length(max=64)])
-    accident_desc = TextAreaField(u'accident desc', validators=[Length(max=128)])
+    contacts = StringField(u'contacts', validators=[DataRequired(message=u'Contacts is required.'),
+                                                    Length(max=16, message=u'contacts out of limitation 32')])
+    tel = StringField(u'tel', validators=[DataRequired(message=u'Phone number is required'),
+                                          Length(min=11, max=11, message=u'Invalid phone number.'),
+                                          Regexp('^\d{11}$', message=u'Invalid phone number.')])
+    store_name = StringField(u'store name', validators=[Length(max=32, message=u'Store name out of limitation 32')])
+    store_addr = StringField(u'store addr', validators=[Length(max=64, message=u'Store addr out of limitation 64')])
+    device_id = StringField(u'device id', validators=[DataRequired(message=u'Device id is required.'),
+                                                      Length(max=32, message=u'Device id out of limitation 32')])
+    accident_desc = TextAreaField(u'accident desc',
+                                  validators=[Length(max=128, message=u'Accident desc out of limitation 128')])
     submit = SubmitField(u'submit')
 
 
--- app/main/view.py
+++ app/main/view.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, flash
 from . import main
 from .form import OrderForm
 
@@ -6,6 +6,11 @@ from .form import OrderForm
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = OrderForm()
-    if form.validate_on_submit():
-        return 'ok'
+    if form.is_submitted():
+        if form.validate():
+            return 'ok'
+        else:
+            form_error = form.errors.items()[0]
+            f_error = form_error[1][0]
+            flash(f_error)
     return render_template('index.html', form=form)
