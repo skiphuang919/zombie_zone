5793069162ebb617bf00136d3bbf969329c9fd41
huangkai <huangkai919@163.com>
2017-03-14 21:35:46 +0800

    add db relationship

--- app/model.py
+++ app/model.py
@@ -2,6 +2,11 @@ from datetime import datetime
 from . import db
 
 
+party_guy_table = db.Table('party_guy_table',
+                           db.Column('user_id', db.String(64), db.ForeignKey('users.user_id')),
+                           db.Column('party_id', db.String(64), db.ForeignKey('parties.party_id')))
+
+
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.String(64), unique=True, index=True, primary_key=True)
@@ -14,9 +19,28 @@ class User(db.Model):
     confirmed = db.Column(db.Boolean, default=False)
     add_time = db.Column(db.DateTime, default=datetime.utcnow())
     update_time = db.Column(db.DateTime)
+    party_guy_table = db.relationship('Parties',
+                                      secondary=party_guy_table,
+                                      backref=db.backref('users', lazy='dynamic'))
 
-    def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
         pass
 
+
+class Parties(db.Model):
+    __tablename__ = 'parties'
+    party_id = db.Column(db.String(64), unique=True, index=True, primary_key=True)
+    subject = db.Column(db.String(128), nullable=False)
+    party_time = db.Column(db.DateTime)
+    address = db.Column(db.String(128), nullable=False)
+    host = db.Column(db.String(64), nullable=False, index=True)
+    required_count = db.Column(db.Integer, nullable=False)
+    note = db.Column(db.String(512))
+    status = db.Column(db.Integer, default=0)
+    create_time = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        super(Parties, self).__init__(*args, **kwargs)
+        pass
 
