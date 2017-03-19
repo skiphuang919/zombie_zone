e62c502f53b801c771f226bea1ab0556cfa600ac
huangkai <huangkai919@163.com>
2017-03-19 23:06:45 +0800

    add party form

--- app/main/form.py
+++ app/main/form.py
@@ -19,5 +19,16 @@ class OrderForm(FlaskForm):
     submit = SubmitField(u'submit')
 
 
-
+class PartyForm(FlaskForm):
+    subject = StringField(u'Subject', validators=[DataRequired(message=u'Subject is required.'),
+                                                  Length(max=32, message=u'Subject out of limitation 32')])
+    party_time = StringField(u'Party time', validators=[DataRequired(message=u'Party time is required')])
+    address = StringField(u'Address', validators=[DataRequired(message=u'Party Address is required'),
+                                                  Length(max=64, message=u'Address out of limitation 64')])
+    required_count = StringField(u'Count',
+                                 validators=[DataRequired(message=u'Require count is required.'),
+                                             Regexp('^\d{1,2}$', message=u'Invalid required count.')])
+    note = TextAreaField(u'Note',
+                         validators=[Length(max=128, message=u'Accident desc out of limitation 128')])
+    submit = SubmitField(u'Submit')
 
--- app/main/view.py
+++ app/main/view.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, current_app, session
+from flask import render_template, flash, redirect, url_for, current_app, session
 from . import main
-from .form import OrderForm
+from .form import OrderForm, PartyForm
 from ..lib import user
 
 
@@ -9,15 +9,22 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/order', methods=['GET', 'POST'])
-def order():
-    form = OrderForm()
+@main.route('/add_party', methods=['GET', 'POST'])
+def add_party():
+    form = PartyForm()
     if form.is_submitted():
         if form.validate():
             # DO SOMETHING HERE
-            return render_template('success.html')
+            return redirect(url_for('main.add_party_success'))
         else:
             form_error = form.errors.items()[0]
             f_error = form_error[1][0]
             flash(f_error)
-    return render_template('order.html', form=form)
+    return render_template('party.html', form=form)
+
+
+@main.route('/add_party_success')
+def add_party_success():
+    return render_template('success.html',
+                           success_title='Success',
+                           success_detail='You have create a party successfully.')
