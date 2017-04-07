c02b53ce6e7fe19e75006314243b87de4fc24d4d
ubuntu16.04 <huangkai919@163.com>
2017-04-07 10:23:40 +0800

    add config file

---
+++ config.py
@@ -0,0 +1,29 @@
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = 'secret_key'
+    APP_ID = 'appid'
+    APP_SECRET = 'app_secret'
+    DEBUG = True
+
+    CACHE_TYPE = 'redis'
+    CACHE_REDIS_HOST = '127.0.0.1'
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = ''
+    CACHE_REDIS_PASSWORD = ''
+
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'MAIL_USERNAME'
+    MAIL_PASSWORD = 'MAIL_PASSWORD'
+    MAIL_DEFAULT_SENDER = 'MAIL_DEFAULT_SENDER'
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+    @staticmethod
+    def init_app(app):
+        pass
