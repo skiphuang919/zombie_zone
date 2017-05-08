b33ac66811748624abf3f962f03baa512593c786
ubuntu16.04 <huangkai919@163.com>
2017-05-08 17:56:02 +0800

    modify tool func

--- app/lib/tools.py
+++ app/lib/tools.py
@@ -10,7 +10,7 @@ def get_db_unique_id():
     return str(uuid.uuid4())
 
 
-def get_datetime_span(origin_dt, days=None, hours=None, seconds=None):
+def get_calculated_datetime(origin_dt, days=None, hours=None, seconds=None):
     """
     calculate a day before base on origin_dt
     if origin_dt is not datetime obj, convert it.
@@ -20,13 +20,13 @@ def get_datetime_span(origin_dt, days=None, hours=None, seconds=None):
     :param seconds: int or float
     :return: datetime obj or None
     """
-
     try:
         origin_dt = datetime.strptime(origin_dt, "%Y-%m-%d %H:%M:%S") if \
             not isinstance(origin_dt, datetime) else origin_dt
         result_dt = origin_dt + timedelta(days=days if days is not None else 0,
                                           hours=hours if hours is not None else 0,
                                           seconds=seconds if seconds is not None else 0)
-        return result_dt
     except Exception as ex:
         print str(ex)
+        result_dt = ''
+    return result_dt
