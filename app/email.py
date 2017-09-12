13100eb483503f6fbdc4913a78e4baf644a21d6e
ubuntu16.04 <huangkai919@163.com>
2017-09-12 15:55:31 +0800

    add reset pwd

--- app/email.py
+++ app/email.py
@@ -5,6 +5,6 @@ from flask import current_app
 def send_confirm_mail(recipient, mail_info):
     task.send_mail.apply_async(args=[current_app.config.get('CONFIRM_MAIL_SUBJECT'),
                                      recipient,
-                                     'confirm_email.html',
+                                     'mail/confirm_email.html',
                                      mail_info],)
 
