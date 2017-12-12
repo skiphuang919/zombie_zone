cc6552d91b10c08f4d2f60374a640253530f42fb
ubuntu16.04 <huangkai919@163.com>
2017-12-12 17:20:20 +0800

    make pagenate common use

--- app/main/view.py
+++ app/main/view.py
@@ -7,6 +7,5 @@ from ..lib import post
 def index():
     page = request.args.get('page', 1, type=int)
     posts_pagination = post.get_paginate_posts(page_num=page)
-    posts = posts_pagination.items
     session['from_endpoint'] = 'main.index'
-    return render_template('index.html', posts=posts, pagination=posts_pagination, top_title='All Posts')
+    return render_template('index.html', pagination=posts_pagination, top_title='All Posts')
